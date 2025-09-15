31dd18d60d666c40238fac6f0c37c9e44aed2164
edwardjwong08 <poiboy@live.com>
2025-09-15 18:12:50 -0400

    modularized files and created makefile commands

---
+++ src/config.py
@@ -0,0 +1,34 @@
+# ---------- SEP projection series (verified FRED IDs) ----------
+SEP_SERIES = {
+    "gdp": { #gdp projections, all not seasonally adjusted
+        "median": "GDPC1MD", #only 3 years out
+        "central_tendency_midpoint": "GDPC1CTM", #only 3 years out
+        "range_midpoint": "GDPC1RM", #only 3 years out
+        "longer_run_central_tendency_midpoint": "GDPC1CTMLR", #at this time includes projections from historical time
+    },
+    "unemployment": { #unemployment projections, all not seasonally adjusted
+        "median": "UNRATEMD", #only 3 years out
+        "central_tendency_midpoint": "UNRATECTM", #only 3 years out
+        "range_midpoint": "UNRATERM", #only 3 years out
+        "longer_run_central_tendency_midpoint": "UNRATECTMLR", #at this time includes projections from historical time
+    },
+    "fed_funds": { #fed funds interest rate projections, all not seasonally adjusted
+        "median": "FEDTARMD", #only 3 years out
+        "central_tendency_midpoint": "FEDTARCTM", #only 3 years out
+        "range_midpoint": "FEDTARRM", #only 3 years out
+        "longer_run_central_tendency_midpoint": "FEDTARCTMLR", #at this time includes projections from historical time
+    },
+    "pce": { #pce inflation projections, all not seasonally adjusted, no longer run CTM
+        "median": "PCECTPIMD", #only 3 years out
+        "central_tendency_midpoint": "PCECTPICTM", #only 3 years out
+        "range_midpoint": "PCECTPIRM" #only 3 years out
+    },
+    "core_pce": { #core pce inflation projections, all not seasonally adjusted, no longer run CTM
+        "median": "JCXFEMD", #only 3 years out
+        "central_tendency_midpoint": "JCXFECTM", #only 3 years out
+        "range_midpoint": "JCXFERM" #only 3 years out
+    }
+}
+
+start_date = "2020-01-01"
+end_date = "2029-01-01"
---
+++ src/fred_utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas_datareader import data as pdr
+
+# ---------- Helper to fetch FRED series ----------
+def fred(code: str, start_date: str, end_date: str) -> pd.Series:
+    # Fetch data from FRED
+    # Inputs: code (str) - FRED series code, start_date (str) - start date desired, end_date (str) - end date desired
+    try:
+        s = pdr.DataReader(code, "fred", start = start_date, end = end_date)[code] #select data during this period
+        s.name = code
+        return s
+    except Exception as e:
+        print(f"Warning: Could not fetch {code} ({e})")
+        return pd.Series(dtype=float)
